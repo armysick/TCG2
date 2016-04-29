b887c75c1b7921d43db69d75d537eecb4b45d315
armysick <uh4v3b33nh4ck3d@gmail.com>
2016-04-29 15:19:29 +0100

    game Menu building

---
+++ app/src/main/java/com/example/ownsick/tcg/gameMenu.java
@@ -0,0 +1,41 @@
+package com.example.ownsick.tcg;
+
+/**
+ * Created by OwnSick on 29-04-2016.
+ */
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.graphics.Color;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.view.MotionEvent;
+import android.transition.TransitionManager;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+public class gameMenu extends Activity{
+
+
+}
--- app/src/main/java/com/example/ownsick/tcg/postLoginMenu.java
+++ app/src/main/java/com/example/ownsick/tcg/postLoginMenu.java
@@ -1,5 +1,7 @@
 package com.example.ownsick.tcg;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,15 +10,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.graphics.Color;
 
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.content.Intent;
+
+import android.view.MotionEvent;
+import android.transition.TransitionManager;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -26,50 +32,63 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class FullscreenActivity extends Activity {
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    Button loginButton;
-    TextView userLabel, passLabel;
-    EditText userTF, passTF;
+/**
+ * Created by OwnSick on 26-04-2016.
+ */
+public class postLoginMenu extends Activity{
+    ViewGroup greetingsLayout;
+    TextView hi;
+    Button playButton;
 
     private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fullscreen);
-        loginButton = (Button)findViewById(R.id.loginButton);
-        userLabel = (TextView)findViewById(R.id.userLabel);
-        passLabel = (TextView)findViewById(R.id.passLabel);
-        userTF = (EditText)findViewById(R.id.userTF);
-        passTF = (EditText)findViewById(R.id.passTF);
+        setContentView(R.layout.activity_postlogin);
+        hi = (TextView)findViewById(R.id.greetingsLabel);
+        playButton = (Button)findViewById(R.id.playButton);
+        greetingsLayout = (ViewGroup)findViewById(R.id.greetingsLayout);
 
-        loginButton.setOnClickListener(new View.OnClickListener(){
+        hi.setOnTouchListener(
+                new RelativeLayout.OnTouchListener(){
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event){
+                        moveLabel();
+                        return true;
+                    }
+                }
+        );
+        playButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v){
-                if(userTF.getText().toString().equals("admin") && passTF.getText().toString().equals("admin")){
-                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
-                    startMainMenu(v);
-
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials..", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(getApplicationContext(), "Starting game...", Toast.LENGTH_SHORT).show();
+                startGame(v);
             }
         });
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
     }
 
-    public void startMainMenu(View view){
+    public void startGame(View v){
         Intent i = new Intent(getApplicationContext(), postLoginMenu.class);
-        view.getContext().startActivity(i);
+        v.getContext().startActivity(i);
+
+    }
+
+    @TargetApi(19)
+    public void moveLabel(){
+        View greetingsView = findViewById(R.id.greetingsLabel);
+        TransitionManager.beginDelayedTransition(greetingsLayout);
+        //position change
+        RelativeLayout.LayoutParams labelposrules = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        labelposrules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        labelposrules.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        greetingsView.setLayoutParams(labelposrules);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
